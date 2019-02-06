1f62ca07dac2f79d656a83b0c661101074630b1d
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-02-06 09:51:44 -0500

    Creating my collection of Pens property.

--- src/com/company/Zoo.java
+++ src/com/company/Zoo.java
@@ -1,4 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zoo {
+
+    private List<Pen> myPens = new ArrayList<>();
+
+    public List<Pen> getMyPens() {
+        return myPens;
+    }
+
+    public void setMyPens(List<Pen> myPens) {
+        this.myPens = myPens;
+    }
 }
