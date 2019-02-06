5cc8a4be3f173d71e09e1c87a2c105337e563e27
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-02-06 09:54:40 -0500

    Creating my collections of Animals and Baby Animals in my Pen class.

--- src/com/company/Pen.java
+++ src/com/company/Pen.java
@@ -1,6 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pen {
 
-    private
+    private List<Animal> myAnimals = new ArrayList<>();
+    private List<BabyAnimal> myBabyAnimals = new ArrayList<>();
+
+    public List<Animal> getMyAnimals() {
+        return myAnimals;
+    }
+
+    public void setMyAnimals(List<Animal> myAnimals) {
+        this.myAnimals = myAnimals;
+    }
+
+    public List<BabyAnimal> getMyBabyAnimals() {
+        return myBabyAnimals;
+    }
+
+    public void setMyBabyAnimals(List<BabyAnimal> myBabyAnimals) {
+        this.myBabyAnimals = myBabyAnimals;
+    }
 }
